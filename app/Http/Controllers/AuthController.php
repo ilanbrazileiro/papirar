28ad14348d380f64c972b561b662dda6d515a4a5
Ilan Brazileiro <ilanbrazileiro@gmail.com>
2026-03-17 15:09:44 -0300

    meu 001

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function logout()
+    {
+        echo "logout";
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        //validação de formulário
+        $request->validate();
+
+        //dd($request);
+        echo $request->input('text_username');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// routes auth
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::get('/logout', [AuthController::class, 'logout']);
+//Route::get('/page2/{value}', [MainController::class, 'page2']);
