80d40f514cbb3871dafb895a4f5ae2a89a8960ee
Ilan Brazileiro <ilanbrazileiro@gmail.com>
2026-08-18 23:10:40 -0300

    ultmo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            function (Request $request, Throwable $e): bool {
+            function (Request $request, \Throwable $e): bool {
                 return $request->is('api/gpt/*') || $request->expectsJson();
             }
         );
